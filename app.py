cc4783c81203ad87660d626820c68081d739e5c6
MANOJ KUMAR <68511922+manojmk29@users.noreply.github.com>
2021-06-08 20:25:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request
 import jsonify
 import requests
 import pickle
-import numpy as np
 app = Flask(__name__)
 model = pickle.load(open('random_forest.pkl', 'rb'))
 @app.route('/',methods=['GET'])
